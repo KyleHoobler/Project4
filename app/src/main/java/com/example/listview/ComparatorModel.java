1c4d357da0e39d53d04a2e7c67772ee727f0de19
xplatypus8 <clifton.dent.15@cnu.edu>
2017-04-08 15:13:17 -0400

    added getModel (unFinished)

---
+++ app/src/main/java/com/example/listview/ComparatorModel.java
@@ -0,0 +1,14 @@
+package com.example.listview;
+
+import java.util.Comparator;
+
+/**
+ * Created by taylo on 4/8/2017.
+ */
+
+public class ComparatorModel implements Comparator<BikeData> {
+        @Override
+        public int compare(BikeData c1, BikeData c2) {
+            return c1.getLocation().compareTo(c2.getLocation());
+        }
+}
